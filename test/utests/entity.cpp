3376bb42fa58d6dce5ba0b01d18c4385cb9a4d45
Mark Marron <mark.marron@uky.edu>
2025-01-13 15:49:01 -0500

    mixed datatype/entity

--- test/utests/entity.cpp
+++ test/utests/entity.cpp
@@ -5,10 +5,10 @@
 #define TEST_IT(TYPE, BSQ) { std::u8string result; std::u8string expected; tround(createMetaPathName("entity"), TYPE, createBSQONPathName("entity", BSQ), expected, result); checkAndReport(result, expected); }
 #define TEST_WITH(TYPE, BSQ, EXPECTED) { std::u8string result; std::u8string orig; tround(createMetaPathName("entity"), TYPE, createBSQONPathName("entity", BSQ), orig, result); checkAndReport(result, EXPECTED); }
 
+BOOST_AUTO_TEST_SUITE(Entity)
+
 std::u8string s_binop1 = u8"Main::BinOp{Main::OpKind#Add, Main::Value{1i}, Main::Value{2i}}";
 std::u8string s_binop2 = u8"Main::BinOp{Main::OpKind#Add, Main::BinOp{Main::OpKind#Sub, Main::Value{3i}, Main::Value{2i}}, Main::Value{2i}}";
-
-BOOST_AUTO_TEST_SUITE(Entity)
 
 ////
 //BaseOps
