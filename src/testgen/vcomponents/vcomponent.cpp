c606b9da684ebac8224bee2dbd5637b0725aa6fa
Mark Marron <mark.marron@uky.edu>
2025-03-07 15:52:11 -0500

    partition code

---
+++ src/testgen/vcomponents/vcomponent.cpp
@@ -0,0 +1,196 @@
+#include "vcomponent.h"
+
+static bsqon::SourcePos g_spos = { 0, 0, 0, 0 };
+
+bool vcpathCMP(const VCPath& p1, const VCPath& p2)
+{
+    if(p1.size() < p2.size()) {
+        return true;
+    }
+    else if(p1.size() > p2.size()) {
+        return false;
+    }
+    else {
+        return p1.compare(p2) < 0;
+    }
+}
+
+ValueSetPartition ValueSetGenerator::generateNone(const bsqon::PrimitiveType* t, const ValueSetGeneratorEnvironment& env)
+{
+    return ValueSetPartition{ {new ValueComponent(env.path, env.constraints, env.context.completeWithValueType(t))} };
+}
+
+ValueSetPartition ValueSetGenerator::generateBool(const bsqon::PrimitiveType* t, const ValueSetGeneratorEnvironment& env)
+{
+    return ValueSetPartition{ {new ValueComponent(env.path, env.constraints, env.context.completeWithValueType(t))} };
+}
+
+ValueSetPartition ValueSetGenerator::generateNat(const bsqon::PrimitiveType* t, const ValueSetGeneratorEnvironment& env)
+{
+    return ValueSetPartition{ {new ValueComponent(env.path, env.constraints, env.context.completeWithValueType(t))} };
+}
+
+ValueSetPartition ValueSetGenerator::generateInt(const bsqon::PrimitiveType* t, const ValueSetGeneratorEnvironment& env)
+{
+    return ValueSetPartition{ {new ValueComponent(env.path, env.constraints, env.context.completeWithValueType(t))} };
+}
+
+//TODO: more primitives..
+
+ValueSetPartition ValueSetGenerator::generatePrimitive(const bsqon::PrimitiveType* t, const ValueSetGeneratorEnvironment& env)
+{
+    auto tk = t->tkey;
+    if(tk == "None") {
+        return this->generateNone(t, env);
+    }
+    else if(tk == "Bool") {
+        return this->generateBool(t, env);
+    }
+    else if(tk == "Nat") {
+        return this->generateNat(t, env);
+    }
+    else if(tk == "Int") {
+        return this->generateInt(t, env);
+    }
+        /*
+        else if(tk == "BigInt") {
+            return this->parseBigInt(t, node);
+        }
+        else if(tk == "BigNat") {
+            return this->parseBigNat(t, node);
+        }
+        else if(tk == "Rational") {
+            return this->parseRational(t, node);
+        }
+        else if(tk == "Float") {
+            return this->parseFloat(t, node);
+        }
+        else if(tk == "Decimal") {
+            return this->parseDecimal(t, node);
+        }
+        else if(tk == "DecimalDegree") {
+            return this->parseDecimalDegree(t, node);
+        }
+        else if(tk == "LatLongCoordinate") {
+            return this->parseLatLong(t, node);
+        }
+        else if(tk == "Complex") {
+            return this->parseComplex(t, node);
+        }
+        else if(tk == "String") {
+            return this->parseString(t, node);
+        }
+        else if(tk == "CString") {
+            return this->parseCString(t, node);
+        }
+        else if(tk == "ByteBuffer") {
+            return this->parseByteBuffer(t, node);
+        }
+        else if(tk == "TZDateTime") {
+            return this->parseTZDateTime(t, node);
+        }
+        else if(tk == "TIATime") {
+            return this->parseTIATime(t, node);
+        }
+        else if(tk == "PlainDate") {
+            return this->parsePlainDate(t, node);
+        }
+        else if(tk == "PlainTime") {
+            return this->parsePlainTime(t, node);
+        }
+        else if(tk == "LogicalTime") {
+            return this->parseLogicalTime(t, node);
+        }
+        else if(tk == "ISOTimeStamp") {
+            return this->parseISOTimeStamp(t, node);
+        }
+        else if(tk == "UUIDv4") {
+            return this->parseUUIDv4(t, node);
+        }
+        else if(tk == "UUIDv7") {
+            return this->parseUUIDv7(t, node);
+        }
+        else if(tk == "SHAContentHash") {
+            return this->parseSHAHashcode(t, node);
+        }
+        else if(tk == "DataTimeDelta") {
+            return this->parseDeltaDateTime(t, node);
+        }
+        else if(tk == "SecondsDelta") {
+            return this->parseDeltaSeconds(t, node);
+        }
+        else if(tk == "ISOTimestampDelta") {
+            return this->parseDeltaISOTimeStamp(t, node);
+        }
+        else if(tk == "LogicalTimeDelta") {
+            return this->parseDeltaLogical(t, node);
+        }      
+        else if(tk == "UnicodeRegex") {
+            return this->parseUnicodeRegex(t, node);
+        }
+        else if(tk == "CRegex") {
+            return this->parseCRegex(t, node);
+        }
+        else if(tk == "PathRegex") {
+            return this->parsePathRegex(t, node);
+        }
+        else if(tk == "Path") {
+            return this->parsePath(t, node);
+        }
+        else if(tk == "PathItem") {
+            return this->parsePathItem(t, node);
+        }
+        else if(tk == "Glob") {
+            return this->parseGlob(t, node);
+        }
+        */
+    else {
+        //Missing primitive type
+        assert(false);
+    }   
+}
+    
+ValueSetPartition ValueSetGenerator::generateEnum(const bsqon::EnumType* t, const ValueSetGeneratorEnvironment& env)
+{
+    return ValueSetPartition{ {new ValueComponent(env.path, env.constraints, env.context.completeWithValueType(t))} };
+}
+
+//More special types here...
+
+ValueSetPartition ValueSetGenerator::generateStdEntityType(const bsqon::StdEntityType* t, const ValueSetGeneratorEnvironment& env)
+{
+    auto tctx = env.context.extendWithEnclosingType(t);
+
+    std::vector<ValueSetPartition> fieldpaths;
+    std::transform(t->fields.cbegin(), t->fields.cend(), std::back_inserter(fieldpaths), [this, &tctx, &env](const bsqon::EntityTypeFieldEntry& f) {
+        auto tenv = env.step(f.fname, env.constraints, tctx.extendForField(f));
+
+        return this->generateType(this->assembly.lookupTypeKey(f.ftype), tenv);
+    });
+}
+
+ValueSetPartition ValueSetGenerator::generateType(const bsqon::Type* t, const ValueSetGeneratorEnvironment& env)
+{
+    switch(t->tag) {
+        case bsqon::TypeTag::TYPE_PRIMITIVE: {
+            return this->generatePrimitive(static_cast<const bsqon::PrimitiveType*>(t), env);
+        }
+        /*
+        * TODO: more tags here
+        */
+        case bsqon::TypeTag::TYPE_STD_ENTITY:
+        {
+            return this->generateStdEntityType(static_cast<const bsqon::StdEntityType*>(t), env);
+        }
+        case bsqon::TypeTag::TYPE_ENUM: {
+            return this->generateEnum(static_cast<const bsqon::EnumType*>(t), env);
+        }
+        /*
+        * TODO: more tags here
+        */
+       default: {
+            //Missing type
+            assert(false);
+        }
+    }
+}
---
+++ src/testgen/vcomponents/vcomponent.h
@@ -0,0 +1,273 @@
+#pragma once
+
+#include "../../common.h"
+
+#include "../../info/type_info.h"
+#include "../../info/bsqon.h"
+
+typedef std::string VCPath;
+
+bool vcpathCMP(const VCPath& p1, const VCPath& p2);
+
+class ValueConstraint
+{
+public:
+    const VCPath path;
+
+    ValueConstraint(VCPath path) : path(path) { ; }
+    virtual ~ValueConstraint() = default;
+
+    virtual std::u8string toString() const = 0;
+};
+
+class MinLengthConstraint : public ValueConstraint
+{
+public:
+    const size_t minlen;
+
+    MinLengthConstraint(VCPath path, size_t minlen) : ValueConstraint(path), minlen(minlen) { ; }
+
+    virtual std::u8string toString() const override
+    {
+        auto sstr = std::to_string(this->minlen);
+        return u8"|" + std::u8string(this->path.cbegin(), this->path.cend()) + u8"| >= " + std::u8string(sstr.cbegin(), sstr.cend());
+    }
+};
+
+class OfTypeConstraint : public ValueConstraint
+{
+public:
+    const bsqon::Type* vtype;
+
+    OfTypeConstraint(VCPath path, const bsqon::Type* vtype) : ValueConstraint(path), vtype(vtype) { ; }
+
+    virtual std::u8string toString() const override
+    {
+        return std::u8string(this->path.cbegin(), this->path.cend()) + u8"?" + std::u8string(this->vtype->tkey.cbegin(), this->vtype->tkey.cend());
+    }
+};
+
+class FixedValueConstraint : public ValueConstraint
+{
+public:
+    const bsqon::Value* value;
+
+    FixedValueConstraint(VCPath path, const bsqon::Value* value) : ValueConstraint(path), value(value) { ; }
+
+    virtual std::u8string toString() const override
+    {
+        return std::u8string(this->path.cbegin(), this->path.cend()) + u8" == " + this->value->toString();
+    }
+};
+
+class GenerateContext
+{
+private:
+    GenerateContext(const std::optional<const bsqon::Type*> intype, std::optional<bsqon::EntityTypeFieldEntry> forfield, const std::optional<const bsqon::TypedeclType*> oftype, const bsqon::Type* valuetype) : intype(intype), forfield(forfield), oftype(oftype), valuetype(valuetype) { ; }
+
+public:
+    std::optional<const bsqon::Type*> intype;
+    std::optional<bsqon::EntityTypeFieldEntry> forfield;
+    std::optional<const bsqon::TypedeclType*> oftype;
+    const bsqon::Type* valuetype;
+
+    GenerateContext() : intype(), forfield(), oftype(), valuetype(nullptr) { ; }
+    GenerateContext(const GenerateContext& other) : intype(other.intype), forfield(other.forfield), oftype(other.oftype), valuetype(other.valuetype) { ; }
+
+    GenerateContext& operator=(const GenerateContext& other)
+    {
+        if(this == &other) {
+            return *this;
+        }
+
+        this->intype = other.intype;
+        this->oftype = other.oftype;
+        this->valuetype = other.valuetype;
+        return *this;
+    }
+
+    std::u8string toString() const
+    {
+        std::u8string its;
+        if(this->intype.has_value()) {
+            its = u8", type=" + std::u8string(this->intype.value()->tkey.cbegin(), this->intype.value()->tkey.cend());
+        }
+
+        std::u8string ffs;
+        if(this->forfield.has_value()) {
+            ffs = u8", field=" + std::u8string(this->forfield.value().fname.cbegin(), this->forfield.value().fname.cend());
+        }
+
+        std::u8string ots;
+        if(this->oftype.has_value()) {
+            ots = u8", oftype=" + std::u8string(this->oftype.value()->tkey.cbegin(), this->oftype.value()->tkey.cend());
+        }
+
+        std::u8string vts = u8"valuetype=" + (this->valuetype != nullptr ? u8"[UNSET]" : std::u8string(this->valuetype->tkey.cbegin(), this->valuetype->tkey.cend()));
+
+        return u8"{ " + its + ffs + ots + vts + u8" }";
+
+    }
+
+    GenerateContext extendWithEnclosingType(const bsqon::Type* t) const
+    {
+        return GenerateContext{std::make_optional(t), this->forfield, this->oftype, nullptr};
+    }
+
+    GenerateContext extendForField(const bsqon::EntityTypeFieldEntry& f) const
+    {
+        return GenerateContext{this->intype, std::make_optional(f), this->oftype, nullptr};
+    }
+
+    GenerateContext extendForTypedecl(const bsqon::TypedeclType* t) const
+    {
+        return GenerateContext{this->intype, this->forfield, std::make_optional(t), nullptr};
+    }
+
+    GenerateContext completeWithValueType(const bsqon::Type* t) const
+    {
+        return GenerateContext{this->intype, this->forfield, this->oftype, t};
+    }
+};
+
+class ValueComponent
+{
+public:
+    const VCPath path;
+    const std::vector<ValueConstraint*> constraints;
+
+    const GenerateContext context;
+    std::vector<bsqon::Value*> options;
+
+    ValueComponent(const VCPath path, const std::vector<ValueConstraint*>& constraints, const GenerateContext& context) : path(path), constraints(constraints), context(context) { ; }
+
+    std::u8string toString() const
+    {
+        auto pathstr = std::u8string(this->path.cbegin(), this->path.cend());
+
+        auto cstr = std::accumulate(this->constraints.cbegin(), this->constraints.cend(), std::u8string{}, [](std::u8string&& a, const ValueConstraint* v) { 
+            return (a.empty() ? u8"" : std::move(a) + u8", ") + v->toString();
+        });
+
+        auto ctx = this->context.toString();
+
+        auto ostr = std::accumulate(this->options.cbegin(), this->options.cend(), std::u8string{}, [](std::u8string&& a, const bsqon::Value* v) { 
+            return (a.empty() ? u8"" : std::move(a) + u8", ") + v->toString(); 
+        });
+
+        auto withstr = cstr.empty() ? u8"" : u8"\n    with { " + ostr + u8" }";
+        auto ctxstr = ctx.empty() ? u8"" : u8" in context" + ctx;
+        
+        return pathstr + u8" => { " + ostr + u8" }" + ctxstr + withstr;
+    }
+};
+
+class ValueSetGeneratorEnvironment
+{
+public:
+    VCPath path;
+    std::vector<ValueConstraint*> constraints;
+    GenerateContext context;
+
+    ValueSetGeneratorEnvironment(const VCPath& path, const std::vector<ValueConstraint*>& constraints, const GenerateContext& context) : path(path), constraints(constraints), context(context) { ; }
+    ValueSetGeneratorEnvironment(const ValueSetGeneratorEnvironment& other) : path(other.path), constraints(other.constraints), context(other.context) { ; }  
+
+    ValueSetGeneratorEnvironment& operator=(const ValueSetGeneratorEnvironment& other)
+    {
+        if(this == &other) {
+            return *this;
+        }
+
+        this->path = other.path;
+        this->constraints = other.constraints;
+        this->context = other.context;
+        return *this;
+    }
+
+    ValueSetGeneratorEnvironment step(const std::string& pathext, const std::vector<ValueConstraint*>& newconstraints, const GenerateContext& updatedcontext) const
+    {
+        VCPath npath = this->path + pathext;
+        std::vector<ValueConstraint*> nconstraints(this->constraints);
+        nconstraints.insert(nconstraints.end(), newconstraints.cbegin(), newconstraints.cend());
+        std::stable_sort(nconstraints.begin(), nconstraints.end(), [](const ValueConstraint* v1, const ValueConstraint* v2) { return vcpathCMP(v1->path, v2->path); });
+
+        return ValueSetGeneratorEnvironment{npath, nconstraints, updatedcontext};
+    }
+};
+
+class ValueSetPartition
+{
+public:
+    std::vector<ValueComponent*> components;
+
+    ValueSetPartition() : components() { ; }
+    ValueSetPartition(std::vector<ValueComponent*>& components) : components(components) { ; }
+    ValueSetPartition(std::vector<ValueComponent*>&& components) : components(std::move(components)) { ; }
+
+    ValueSetPartition& operator=(const ValueSetPartition& other)
+    {
+        if(this == &other) {
+            return *this;
+        }
+
+        this->components = other.components;
+        return *this;
+    }
+
+    ValueSetPartition& operator=(const ValueSetPartition&& other)
+    {
+        if(this == &other) {
+            return *this;
+        }
+
+        this->components = std::move(other.components);
+        return *this;
+    }
+
+    std::u8string toString() const
+    {
+        auto cstr = std::accumulate(this->components.cbegin(), this->components.cend(), std::u8string{}, [](std::u8string&& a, const ValueComponent* v) { 
+            return (a.empty() ? u8"" : std::move(a) + u8"\n") + v->toString();
+        });
+
+        return cstr;
+    }
+
+    static ValueSetPartition punion(const std::vector<ValueSetPartition>& partitions)
+    {
+        std::vector<ValueComponent*> components;
+        for(size_t i = 0; i < partitions.size(); ++i) {
+            components.insert(components.end(), partitions[i].components.cbegin(), partitions[i].components.cend());
+        }
+        std::stable_sort(components.begin(), components.end(), [](const ValueComponent* v1, const ValueComponent* v2) { return vcpathCMP(v1->path, v2->path); });
+
+        return ValueSetPartition{std::move(components)};
+    }
+};
+
+class ValueSetGenerator
+{
+public:
+    bsqon::AssemblyInfo assembly;
+
+    ValueSetGenerator() { ; }
+    ~ValueSetGenerator() { ; }
+
+    ValueSetPartition generateNone(const bsqon::PrimitiveType* t, const ValueSetGeneratorEnvironment& env);
+
+    ValueSetPartition generateBool(const bsqon::PrimitiveType* t, const ValueSetGeneratorEnvironment& env);
+    ValueSetPartition generateNat(const bsqon::PrimitiveType* t, const ValueSetGeneratorEnvironment& env);
+    ValueSetPartition generateInt(const bsqon::PrimitiveType* t, const ValueSetGeneratorEnvironment& env);
+
+    //TODO: more primitives..
+
+    ValueSetPartition generatePrimitive(const bsqon::PrimitiveType* t, const ValueSetGeneratorEnvironment& env);
+    ValueSetPartition generateEnum(const bsqon::EnumType* t, const ValueSetGeneratorEnvironment& env);
+
+    //More special types here...
+
+    ValueSetPartition generateStdEntityType(const bsqon::StdEntityType* t, const ValueSetGeneratorEnvironment& env);
+
+    ValueSetPartition generateType(const bsqon::Type* t, const ValueSetGeneratorEnvironment& env);
+};
+
